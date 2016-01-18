02bcd27a9391c51c2dbff38f97603c82c83cbf4b
Abhi1690 <abhidas.prof@gmail.com>
2016-01-18 13:22:21 +0530

    My first python file

---
+++ helloworld.py
@@ -0,0 +1,4 @@
+print("hello world")
+friends = ['john', 'pat', 'gary', 'michael']
+for i, name in enumerate(friends):
+    print "friends are {iteration} is {name}".format(iteration=i+1, name=name)
